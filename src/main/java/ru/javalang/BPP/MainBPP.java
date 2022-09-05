385d7d91447738f48eb83de9072e817991d18336
IGIngvar <obezianka98@mail.ru>
2022-09-05 14:08:07 +0400

    4

--- src/main/java/ru/javalang/BPP/MainBPP.java
+++ src/main/java/ru/javalang/BPP/MainBPP.java
@@ -4,12 +4,19 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import ru.javalang.BPP.Currency.CurrencyBeanPostProcessor;
 import ru.javalang.BPP.Currency.Dollar;
+import ru.javalang.BPP.second.ColorFactory;
+import ru.javalang.BPP.second.InjectRandomIntBeanPostProcessor;
+import ru.javalang.BPP.second.MyBean;
 
 public class MainBPP {
     public static void main(String[] args) {
 
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(Dollar.class, CurrencyBeanPostProcessor.class);
+        ApplicationContext applicationContext1 = new AnnotationConfigApplicationContext(InjectRandomIntBeanPostProcessor.class, MyBean.class, ColorFactory.class);
+        System.out.println(applicationContext1.getBean(MyBean.class));
         System.out.println(applicationContext.getBean(Dollar.class));
+
+
 
     }
 }
